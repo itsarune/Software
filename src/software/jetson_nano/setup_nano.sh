8c982fa2a70aac60196f6c795ef8caaf27310e41
arun <arun.sfc@outlook.com>
2024-06-08 13:20:09 -0700

    wip

--- src/software/jetson_nano/setup_nano.sh
+++ src/software/jetson_nano/setup_nano.sh
@@ -4,13 +4,10 @@ set -ex
 
 host_software_packages=(
     redis
-    libjpeg8-dev
-    libjpeg-dev
     device-tree-compiler
     python3.8      # Python 3
     python3.8-venv # Virtual Environment
     python3.8-dev  # Python 3 Headers
-    zlib1g-dev
     curl
 )
 
@@ -29,14 +26,6 @@ sudo /opt/tbotspython/bin/python3 -m pip install --upgrade pip
 
 pip_libaries=(
     setuptools==60.5.0
-    Adafruit-Blinka==6.15.0
-    adafruit-circuitpython-busdevice==5.1.1
-    adafruit-circuitpython-rgb-display==3.10.9
-    Adafruit-PlatformDetect==3.18.0
-    Adafruit-PureIO==1.1.9
-    Jetson.GPIO==2.0.17
-    Pillow==9.1.0
-    redis==4.1.4
     platformio==6.0.2
 )
 
