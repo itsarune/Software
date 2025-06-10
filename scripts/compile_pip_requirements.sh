378f1797bc1613f8c05c1b8763f8dca3d0a3e920
Arun <arun.sfc@outlook.com>
2025-06-10 14:22:23 -0700

    still working on why pytests launch old protobuf version

--- scripts/compile_pip_requirements.sh
+++ scripts/compile_pip_requirements.sh
@@ -16,6 +16,7 @@ BAZEL_ROOT_DIR="$CURR_DIR/../src"
 
 cd $BAZEL_ROOT_DIR
 bazel run //extlibs:nanopb_requirements.update
+bazel run //proto:requirements.update
 bazel run //software/thunderscope:requirements.update
 bazel run //software/embedded/ansible:requirements.update
 bazel run //software/simulated_tests:requirements.update
